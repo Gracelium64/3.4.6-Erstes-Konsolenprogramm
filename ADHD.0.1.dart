f26d467d243526bfd4a13507d25bb0b8ffc6bc54
Grace <grace6424@icloud.com>
2025-03-07 18:59:04 +0100

    UI/UX

--- ADHD.0.1.dart
+++ ADHD.0.1.dart
@@ -3,82 +3,146 @@ import 'dart:io';
 
 void main () {
 
-  int? userInputNumber = null;
-  int? input = 0;
+  String? userInput = null;
+  String dblPnt = ':';
+  // int? input = 0;
 
-  Map<int, String> mainMenu = {
-    1 : 'View List',
-    2 : 'Complete a task',
-    3 : 'Add a Task',
-    4 : 'Delete a task',
-    666 : 'End program from every menu',
+  Map<String, String> mainMenu = {
+    '(a)' : 'View List',
+    '(b)' : 'Complete a task',
+    '(c)' : 'Add a Task',
+    '(d)' : 'Delete a task',
+    '(e)' : 'End program',
   };
   
-  Map<int, String> toDoList = {
-    1 : 'Add a task',
-    2 : 'Delete a Task',
+  Map<String, String> toDoList = {
+    '1' : 'Start by adding a task',
+    '2' : 'And than mark the first task as completed',
+    '3' : 'sdg',
   };
 
-// // // // // // // // // Splash Screen // // // // // // // // //
-        // print("\x1B[2J\x1B[0;0H");
-        // print('Initializing');
-        // sleep(const Duration(milliseconds: 300));
-        // print("\x1B[2J\x1B[0;0H");
-        // print('Initializing');
-        // sleep(const Duration(milliseconds: 300));
-        // print("\x1B[2J\x1B[0;0H");
-        // print('Initializing.');
-        // sleep(const Duration(milliseconds: 300));
-        // print("\x1B[2J\x1B[0;0H");
-        // print('Initializing..');
-        // sleep(const Duration(milliseconds: 300));
-        // print("\x1B[2J\x1B[0;0H");
-        // print('Initializing...');
-        // sleep(const Duration(milliseconds: 300));
-        // print("\x1B[2J\x1B[0;0H");
-        // print('Initializing....');
-        // sleep(const Duration(milliseconds: 500));
-        // print("\x1B[2J\x1B[0;0H");
-        // sleep(const Duration(seconds: 2));
-        // print('Organic Interface Studios');
-        // sleep(const Duration(seconds: 3));
-        // print('Attention Deficit oH Dear v0.1');
-        // sleep(const Duration(seconds: 3));
-        // print("\x1B[2J\x1B[0;0H");
-        // sleep(const Duration(seconds: 2));
+// // // // // // // // Splash Screen // // // // // // // // //
+        print("\x1B[2J\x1B[0;0H");
+        print('Initializing');
+        sleep(const Duration(milliseconds: 300));
+        print("\x1B[2J\x1B[0;0H");
+        print('Initializing');
+        sleep(const Duration(milliseconds: 300));
+        print("\x1B[2J\x1B[0;0H");
+        print('Initializing.');
+        sleep(const Duration(milliseconds: 300));
+        print("\x1B[2J\x1B[0;0H");
+        print('Initializing..');
+        sleep(const Duration(milliseconds: 300));
+        print("\x1B[2J\x1B[0;0H");
+        print('Initializing...');
+        sleep(const Duration(milliseconds: 300));
+        print("\x1B[2J\x1B[0;0H");
+        print('Initializing....');
+        sleep(const Duration(milliseconds: 500));
+        print("\x1B[2J\x1B[0;0H");
+        sleep(const Duration(seconds: 2));
+        print('Organic Interface Studios');
+        sleep(const Duration(seconds: 3));
+        print('Attention Deficit oH Dear v0.1');
+        sleep(const Duration(seconds: 3));
+        print("\x1B[2J\x1B[0;0H");
+        sleep(const Duration(seconds: 2));
 
 // // // // // // // // // Main Menu // // // // // // // // //
                                                               print("\x1B[2J\x1B[0;0H");
   print('Hello adventurer, what would you like to do?');
-  print('Type a number to choose an action');
+  // print('Type a number to choose an action');
   print('');
   print('Main Menu');
-  // sleep(const Duration(seconds: 2));
+  sleep(const Duration(milliseconds: 500));
     for (final allPrint in mainMenu.entries) {
       print('${allPrint.key}: ${allPrint.value}');
   }
   
-  while (userInputNumber == null) {
-  userInputNumber = int.tryParse(stdin.readLineSync() ?? '');
+  while (userInput == null) {
+  userInput = stdin.readLineSync();
   
   
-    if (userInputNumber == null) {
-    print('This is not a number');
-    print('Please try again');
+    if (userInput == null) {
+    print('a');
     } else {
-  switch (userInputNumber) {
-    case 1 : 
-    userInputNumber = null;
+  switch (userInput) {
+    case 'a' : 
+    userInput = null;
+    print("\x1B[2J\x1B[0;0H");
+    sleep(const Duration(milliseconds: 300));
+    print('Quests available:');
+    print('${toDoList.length} / 10');
+    print('');
             for (final allPrint in toDoList.entries)
-            {print('${allPrint.key}: ${allPrint.value}');
+            { 
+              print('${allPrint.key}'+' $dblPnt '+' ${allPrint.value}');
             }
-    // case 2 : _ ;
-    // case 3 : _ ;
-    // case 4 : _ ;
-    case 666 : print('Ok now bye bye than!');
-    default : userInputNumber = null;
+            print('');
+            print('');
+            for (final allPrint in mainMenu.entries) {
+      print('${allPrint.key}: ${allPrint.value}');
+  }
+    case 'b' :  
+    userInput = null;
+    print("\x1B[2J\x1B[0;0H");
+    sleep(const Duration(milliseconds: 300));
+    print('Which good fortune have you brought from your adventures?');
+    print('Which tasks have you completed?');
+    print('');
+            for (final allPrint in toDoList.entries)
+            { 
+              print('${allPrint.key}'+' $dblPnt '+' ${allPrint.value}');
+            }
+            print('');
+            print('');
+            for (final allPrint in mainMenu.entries) {
+      print('${allPrint.key}: ${allPrint.value}');
+  } ;
+    case 'c' :  
+    userInput = null;
+    print("\x1B[2J\x1B[0;0H");
+    sleep(const Duration(milliseconds: 300));
+    print('Enter name of new adventure');
+    print('');
+    //             for (final allPrint in toDoList.entries)
+    //         { 
+    //           print('${allPrint.key}'+' $dblPnt '+' ${allPrint.value}');
+    //         }
+            print('');
+            print('');
+            for (final allPrint in mainMenu.entries) {
+      print('${allPrint.key}: ${allPrint.value}');
+  } ;
+    case 'd' :  
+    userInput = null;
+    print("\x1B[2J\x1B[0;0H");
+    sleep(const Duration(milliseconds: 300));
+    print('Only a fool ignores an adventure');
+    print('Oh well, what quest has failed you?');
+    print('');
+                for (final allPrint in toDoList.entries)
+            { 
+              print('${allPrint.key}'+' $dblPnt '+' ${allPrint.value}');
+            }
+            print('');
+            print('');
+            for (final allPrint in mainMenu.entries) {
+      print('${allPrint.key}: ${allPrint.value}');
+  } ;
+    case 'e' : print("\x1B[2J\x1B[0;0H");
+    sleep(const Duration(milliseconds: 300));
+    print('Ok now bye bye than!');
+        sleep(const Duration(seconds: 2));
+        print("\x1B[2J\x1B[0;0H");
+    case '1' : //delete this number of entry from list
+              //back to main menu print 
+              // X 10
+    
+    default : userInput = null;
               print('Please type a valid number from the list darling');
-       }
+           }
     }
   }
 
