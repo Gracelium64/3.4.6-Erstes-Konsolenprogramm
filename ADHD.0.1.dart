d3c3dd767f6130a43d12acbef71079724f232be4
Grace <grace6424@icloud.com>
2025-03-09 11:11:07 +0100

    looks like meat is back on the menu

--- ADHD.0.1.dart
+++ ADHD.0.1.dart
@@ -15,36 +15,36 @@ void main () {
   int taskId = toDoList.length;
 
 // // // // // // // // Splash Screen // // // // // // // // //
-        // print("\x1B[2J\x1B[0;0H");
-        // print('Initializing');
-        // sleep(const Duration(milliseconds: 300));
-        // print("\x1B[2J\x1B[0;0H");
-        // print('Initializing');
-        // sleep(const Duration(milliseconds: 300));
-        // print("\x1B[2J\x1B[0;0H");
-        // print('Initializing.');
-        // sleep(const Duration(milliseconds: 300));
-        // print("\x1B[2J\x1B[0;0H");
-        // print('Initializing..');
-        // sleep(const Duration(milliseconds: 300));
-        // print("\x1B[2J\x1B[0;0H");
-        // print('Initializing...');
-        // sleep(const Duration(milliseconds: 300));
-        // print("\x1B[2J\x1B[0;0H");
-        // print('Initializing....');
-        // sleep(const Duration(milliseconds: 500));
-        // print("\x1B[2J\x1B[0;0H");
-        // sleep(const Duration(seconds: 2));
-        // print('Organic Interface Studios');
-        // sleep(const Duration(seconds: 3));
-        // print('Attention Deficit oH Dear v0.1');
-        // print('Gamify your life');
-        // sleep(const Duration(seconds: 3));
-        // print("\x1B[2J\x1B[0;0H");
-        // sleep(const Duration(seconds: 2));
+        print("\x1B[2J\x1B[0;0H");
+        print('Initializing');
+        sleep(const Duration(milliseconds: 300));
+        print("\x1B[2J\x1B[0;0H");
+        print('Initializing');
+        sleep(const Duration(milliseconds: 300));
+        print("\x1B[2J\x1B[0;0H");
+        print('Initializing.');
+        sleep(const Duration(milliseconds: 300));
+        print("\x1B[2J\x1B[0;0H");
+        print('Initializing..');
+        sleep(const Duration(milliseconds: 300));
+        print("\x1B[2J\x1B[0;0H");
+        print('Initializing...');
+        sleep(const Duration(milliseconds: 300));
+        print("\x1B[2J\x1B[0;0H");
+        print('Initializing....');
+        sleep(const Duration(milliseconds: 500));
+        print("\x1B[2J\x1B[0;0H");
+        sleep(const Duration(seconds: 2));
+        print('Organic Interface Studios');
+        sleep(const Duration(seconds: 3));
+        print('Attention Deficit oH Dear v0.1');
+        print('Gamify your life');
+        sleep(const Duration(seconds: 3));
+        print("\x1B[2J\x1B[0;0H");
+        sleep(const Duration(seconds: 2));
 
 // // // // // // // // // Main Menu // // // // // // // // //
-                                                              print("\x1B[2J\x1B[0;0H");
+  print("\x1B[2J\x1B[0;0H");
   print('Hello adventurer, what would you like to do?');
   print('');
   print('Main Menu');
@@ -98,7 +98,19 @@ void main () {
             int? idDelete = int.tryParse(stdin.readLineSync() ?? '');
               if (toDoList.containsKey(idDelete)) {
                   toDoList.remove(idDelete);
-                  // taskId--;
+// {
+// https://stackoverflow.com/questions/79495595/automatically-assigning-mapint-string-key-after-deletion-of-another-key-in-da/79495638#79495638
+                  final Map<int, String> newTodoList = {};
+                  int newIndex = 1;
+                  toDoList.forEach((key, value) {
+                  newTodoList[newIndex] = value;
+                  newIndex++;
+                  }
+                );
+                  toDoList.clear();
+                  toDoList.addAll(newTodoList);     
+//crude implemintation         
+// }
                   print('Good Job!');
                   sleep(const Duration(milliseconds: 1000));
                   print("\x1B[2J\x1B[0;0H");
@@ -122,12 +134,24 @@ void main () {
       sleep(const Duration(milliseconds: 300));
       print("\x1B[2J\x1B[0;0H");
       print('Quest added!');
-        // taskId++;
       sleep(const Duration(milliseconds: 1000));
       print("\x1B[2J\x1B[0;0H");
               for (final allPrint in mainMenu.entries) {
                 print('${allPrint.key}: ${allPrint.value}');
-              } ;
+              } 
+// {
+// https://stackoverflow.com/questions/79495595/automatically-assigning-mapint-string-key-after-deletion-of-another-key-in-da/79495638#79495638
+                  final Map<int, String> newTodoList = {};
+                  int newIndex = 1;
+                  toDoList.forEach((key, value) {
+                  newTodoList[newIndex] = value;
+                  newIndex++;
+                  }
+                );
+                  toDoList.clear();
+                  toDoList.addAll(newTodoList);     
+//crude implemintation         
+// }
     case 'd' : print("\x1B[2J\x1B[0;0H");
       sleep(const Duration(milliseconds: 300));
       print('Ok now bye bye then!');
